f2a8c4d6c26521b07faaeff9ef24141f2f22b6b3
Nikita Nikishin <nnikishi@emich.edu>
2012-01-15 14:35:12 -0500

    Added logic to prevent shellinabox from starting twice.

--- server.py
+++ server.py
@@ -2,14 +2,15 @@ from flask import Flask, url_for, render_template
 from terminal import Terminal
 
 app = Flask(__name__)
-
-terminal = Terminal()
-terminal.start()
+shellinabox = Terminal()
 
 @app.route('/terminal')
 def terminal():
   return render_template('terminal.html')
 
 if __name__ == "__main__":
+  if not shellinabox.process:
+    shellinabox.start()
+  
   app.debug = True
   app.run(port=5001)
